9cef4cf51644dd39fc7a0cb5ac31e4c5229b9e31
Brian Skocir <brianskocir262@gmail.com>
2023-04-03 18:17:14 -0300

    Segunda entrega curso JS

---
+++ Producto.js
@@ -0,0 +1,10 @@
+class Producto {
+    constructor (id, nombre, categoria, precio, stock){
+        this.id = id
+        this. nombre = nombre
+        this. categoria = categoria
+        this. precio = precio
+        this. stock = stock
+        this. vendido = false
+    }
+    }
